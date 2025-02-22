f57494a2229eace391294ae853b26155d9d21794
tipusahil <tipusahil.ctg@gmail.com>
2025-02-22 09:08:33 +0600

    callback function running

--- callback.js
+++ callback.js
@@ -1,4 +1,26 @@
 function mainFunc1( callbackFunc , name){
-    callbackFunc();
+    callbackFunc(name);
 }
 
+function goodMorning(name){//uporer etai holo main function 
+    console.log(`good morning ${name}`);
+}
+
+const callback1 = mainFunc1( goodMorning , 'shukkor vai')
+// 
+
+// callback function 2
+function goodEvening(name){
+    console.log(`good evening, ${name}`)
+}
+
+
+const callback2 = mainFunc1(goodEvening , 'abdullah')
+
+
+// ei  callback ta eventlistener diye kora hoi jmn:
+function callbackFunctionWithoutBrackets(){
+    console.log(`amk call back function er jonno banano hoise.`)
+}
+
+document.addEventListener('click' , callbackFunctionWithoutBrackets)
