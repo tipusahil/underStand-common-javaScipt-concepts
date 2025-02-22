dc24fdab0c21cc2ea68fa3947dff89a00734461e
tipusahil <tipusahil.ctg@gmail.com>
2025-02-22 09:18:34 +0600

    callback function running

--- callback.js
+++ callback.js
@@ -1,3 +1,9 @@
+/* 
+eakne ja kora hoise, je karone callback function bola hocce ta holo:
+ ekta main function create kora hoise, otar peramitar er (argument) hisebe arekta functionName pass kora hocce (without bracket.. jevabe eventlistener e callback kora hoi). 
+//  normally ekta function er peramitar hisebe (string,boolean,number,function,array,object) sob kisu pass kora jai, so jkn function pass kora hoi tkn bracket chra pass korte hoi, & eta kaj aro sohoj kore dei, & main function k templete er moto kore use kora jai.
+*/
+
 function mainFunc1( callbackFunc , name){
     callbackFunc(name);
 }
