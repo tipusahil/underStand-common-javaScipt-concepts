f9461e450985c94326882b94f4c24a1a77dd26ca
tipusahil <tipusahil.ctg@gmail.com>
2025-02-22 09:23:07 +0600

    callback function running

--- callback.js
+++ callback.js
@@ -4,29 +4,32 @@ eakne ja kora hoise, je karone callback function bola hocce ta holo:
 //  normally ekta function er peramitar hisebe (string,boolean,number,function,array,object) sob kisu pass kora jai, so jkn function pass kora hoi tkn bracket chra pass korte hoi, & eta kaj aro sohoj kore dei, & main function k templete er moto kore use kora jai.
 */
 
-function mainFunc1( callbackFunc , name){
+function mainFunc1(callbackFunc, name) {
     callbackFunc(name);
 }
 
-function goodMorning(name){//uporer etai holo main function 
+function goodMorning(name) {//uporer etai holo main function 
     console.log(`good morning ${name}`);
 }
 
-const callback1 = mainFunc1( goodMorning , 'shukkor vai')
-// 
+mainFunc1(goodMorning, 'shukkor vai')//  good morning shukkor vai
+mainFunc1(goodMorning, 'hisbullah vai')// good morning hisbullah vai 
+/* */
+
 
 // callback function 2
-function goodEvening(name){
+function goodEvening(name) {
     console.log(`good evening, ${name}`)
 }
 
 
-const callback2 = mainFunc1(goodEvening , 'abdullah')
+mainFunc1(goodEvening, 'abdullah'); // good evening, abdullah
+mainFunc1(goodEvening, 'muhammad') ;//good evening, muhammad
 
 
 // ei  callback ta eventlistener diye kora hoi jmn:
-function callbackFunctionWithoutBrackets(){
-    console.log(`amk call back function er jonno banano hoise.`)
-}
+// function callbackFunctionWithoutBrackets() {
+//     console.log(`amk call back function er jonno banano hoise.`)
+// }
 
-document.addEventListener('click' , callbackFunctionWithoutBrackets)
+// document.addEventListener('click', callbackFunctionWithoutBrackets)
